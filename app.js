57af11a48cc05f38d2cdfff221bd14dfb21d804a
Viren Bhagat <virenb@gmail.com>
2017-11-05 21:38:07 -0500

    Added some basic structure and content

---
+++ app.js
@@ -0,0 +1,21 @@
+const streams = [
+  "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp",
+  "https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2",
+  "https://wind-bow.gomix.me/twitch-api/streams/OgamingSC2",
+  "https://wind-bow.gomix.me/twitch-api/streams/cretetion",
+  "https://wind-bow.gomix.me/twitch-api/streams/storbeck",
+  "https://wind-bow.gomix.me/twitch-api/streams/habathcx",
+  "https://wind-bow.gomix.me/twitch-api/streams/RobotCaleb",
+  "https://wind-bow.gomix.me/twitch-api/streams/noobs2ninjas"
+];
+
+const twitchChannels = [
+  "https://go.twitch.tv/freecodecamp",
+  "https://go.twitch.tv/ESL_SC2",
+  "https://go.twitch.tv/OgamingSC2",
+  "https://go.twitch.tv/cretetion",
+  "https://go.twitch.tv/storbeck",
+  "https://go.twitch.tv/habathcx",
+  "https://go.twitch.tv/RobotCaleb",
+  "https://go.twitch.tv/noobs2ninjas"
+]
